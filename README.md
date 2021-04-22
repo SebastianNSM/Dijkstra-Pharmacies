aa3516b44a6dbde3960daa83cf539ba60fcf529e
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-04-21 21:31:30 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# To Do
+
+1. Adjacency list from pharmacy
+2. Calculate the Shortest path from node to node
+3. Search location by hash
---
+++ src/main/java/com/jes/pharmacygraph/PharmacyGraphApplication.java
@@ -0,0 +1,13 @@
+package com.jes.pharmacygraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyGraphApplication.class, args);
+    }
+
+}
